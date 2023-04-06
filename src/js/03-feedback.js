b0f1f9275b754b0d2437c8a716ecc5cab5ab32e0
aggenov <a.g.genov@gmail.com>
2023-04-06 14:25:11 +0300

    bugfix HW save enable value

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -4,37 +4,35 @@ import { save, load, remove } from './storage';
 const filterForm = document.querySelector('.feedback-form');
 const LOCAL_STORAGE_KEY = 'feedback-form-state';
 
-const handleSubmit = evt => {
-  evt.preventDefault();
+// опрос хранилища при загрузке и внесение данных в поля формы // initForm();
 
-  const {
-    elements: { email, message },
-  } = evt.currentTarget;
-
-  console.log({ email: email.value, message: message.value });
-  evt.currentTarget.reset();
-  remove(LOCAL_STORAGE_KEY);
-};
-
-filterForm.addEventListener('submit', handleSubmit);
-
-// initForm();
 (function initForm() {
   const saveData = load(LOCAL_STORAGE_KEY);
-  //   console.log(saveData);
 
   if (!saveData) {
+    const obj = { email: '', message: '' }; // создание объекта полей с пустыми значениями для записи в хранилище
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(obj));
     return;
   }
-  //   Object.entries(saveData).forEach(([name, value]) => {
-  //     filterForm.elements[name].value = value;
-  //   });
   filterForm.email.value = saveData.email;
   filterForm.message.value = saveData.message;
 })();
 
+// вывод в консоль объекта + очистка полей формы и хранилища
+const handleSubmit = evt => {
+  evt.preventDefault();
+  const {
+    elements: { email, message },
+  } = evt.currentTarget;
+  console.log({ email: email.value, message: message.value });
+  evt.currentTarget.reset(); // очистка полей формы
+  remove(LOCAL_STORAGE_KEY); // очистка хранилища
+  location.reload(); // перезагрузка страницы
+};
+
+// обновление хранилища
 const onFormInput = evt => {
-  const { name = '', value = '' } = evt.target;
+  const { name, value } = evt.target;
 
   let saveData = load(LOCAL_STORAGE_KEY);
   saveData = saveData ? saveData : {};
@@ -44,5 +42,7 @@ const onFormInput = evt => {
   save(LOCAL_STORAGE_KEY, saveData);
 };
 
-const throttleOnFormInput = throttle(onFormInput, 500);
-filterForm.addEventListener('input', throttleOnFormInput);
+filterForm.addEventListener('submit', handleSubmit);
+
+const throttleOnFormInput = throttle(onFormInput, 500); // задержка в 500мс на обновление хранилища
+filterForm.addEventListener('input', throttleOnFormInput); //слушатель ввода в поля формы
